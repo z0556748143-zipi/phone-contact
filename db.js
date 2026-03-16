fd1f3eceb390b89f805daf9d96856787f4d68e35
zipiL <y0506282605@gmail.com>
2026-03-17 01:59:21 +0200

    show in console thevalue of select

--- db.js
+++ db.js
@@ -22,6 +22,7 @@ anshey_Kesher.push({ id: Date.now(),name: name, phone: phone });
 const createContactElement = (someOne) => {
     const conta = document.createElement("div");
     conta.textContent=`name: ${someOne.name}  phone: ${someOne.phone}`;
+
 const select = document.createElement("select");
 const categories = [
         { text: "משפחה", emoji: "🏠" },
@@ -29,15 +30,21 @@ const categories = [
         { text: "חברים", emoji: "🎉" }
     ];
 
-    // 5. הלולאה שמכניסה את האופציות לתוך ה-Select
+    //  הלולאה שמכניסה את האופציות לתוך ה-Select
     for (const item of categories) {
         const opt = document.createElement("option");
         opt.value = item.emoji;
         opt.textContent = item.text;
         select.append(opt); // פה האופציה נכנסת לתוך ה-select
     }
+select.addEventListener('change', () => {
+    someOne.category = select.value;
+    console.log("המערך המעודכן:", anshey_Kesher);
+});
+
     // עכשיו conta מכיל גם את הטקסט וגם את התיבה
-    conta.append(select);   
+    conta.append(select); 
+
     return conta;
 };
 
