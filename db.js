efe44182d3ced919cb5a897bd28810ebaf313b05
zipiL <y0506282605@gmail.com>
2026-03-17 01:45:57 +0200

    options in db with emojii

--- db.js
+++ db.js
@@ -22,8 +22,25 @@ anshey_Kesher.push({ id: Date.now(),name: name, phone: phone });
 const createContactElement = (someOne) => {
     const conta = document.createElement("div");
     conta.textContent=`name: ${someOne.name}  phone: ${someOne.phone}`;
+const select = document.createElement("select");
+const categories = [
+        { text: "משפחה", emoji: "🏠" },
+        { text: "עבודה", emoji: "👨‍💻" },
+        { text: "חברים", emoji: "🎉" }
+    ];
+
+    // 5. הלולאה שמכניסה את האופציות לתוך ה-Select
+    for (const item of categories) {
+        const opt = document.createElement("option");
+        opt.value = item.emoji;
+        opt.textContent = item.text;
+        select.append(opt); // פה האופציה נכנסת לתוך ה-select
+    }
+    // עכשיו conta מכיל גם את הטקסט וגם את התיבה
+    conta.append(select);   
     return conta;
-}
+};
+
 export const render =()=>{
     return anshey_Kesher.map(createContactElement);
 };
